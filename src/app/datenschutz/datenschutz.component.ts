5aa9383a22d9a6a4c258231b9505d2548af06223
th62 <toha@uni-bremen.de>
2023-03-23 11:12:56 +0100

    impressum und datenschutz integriert

--- src/app/datenschutz/datenschutz.component.ts
+++ src/app/datenschutz/datenschutz.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormatterService} from "../_services/formatter.service";
+import {UserService} from "../_services/user.service";
 
 @Component({
   selector: 'app-datenschutz',
   templateUrl: './datenschutz.component.html',
   styleUrls: ['./datenschutz.component.css']
 })
-export class DatenschutzComponent {
+export class DatenschutzComponent implements OnInit {
 
+  constructor(private userService: UserService, public formatter: FormatterService) { }
+
+  ngOnInit(): void { }
 }
--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormatterService} from "../_services/formatter.service";
 
 @Component({
   selector: 'app-impressum',
   templateUrl: './impressum.component.html',
   styleUrls: ['./impressum.component.css']
 })
-export class ImpressumComponent {
+export class ImpressumComponent  implements OnInit {
+  constructor(public formatter: FormatterService) {
+  }
+
+  ngOnInit(): void {
+  }
 
 }
